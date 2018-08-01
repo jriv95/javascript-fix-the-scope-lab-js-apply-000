d8624ae5e54a2cd9ff14af401a16ae401613f5cd
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-01 02:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  if(animal == "dog")
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  return "cat"
 }
 
 const two = 2
