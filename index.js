d44b915d41ffb87b446c4fb016ab376978328f5a
Joel Rivera <joel.rivera7995@gmail.com>
2018-08-01 02:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,12 +11,16 @@ function yourAnimal() {
   return animal
 }
 
+const two = 2
+}
+
+var n = 790
+
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
-}
+
 
 var funkyFunction = function() {
   return function() {
